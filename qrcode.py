5f65892210c29ff948fdc424b9054df4ead7b2e2
Zarlykov Baiel <88328136+zarlykovbaiel@users.noreply.github.com>
2022-01-08 15:14:39 +0600

    Delete qrcode.py

--- qrcode.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-# пример данных
-link  = "https://files.fm/f/qzvfsp9eg"
-# имя конечного файла
-filename = "aiprint1.png"
-# генерируем qr код
-img  = qrcode.make(link)
-# сохраняем img в файл
-img.save(filename)
