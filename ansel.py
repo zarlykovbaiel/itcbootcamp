a660d206cd151f15c6bd9a799a74d09989b01813
Zarlykov Baiel <88328136+zarlykovbaiel@users.noreply.github.com>
2022-01-10 12:32:56 +0600

    print

---
+++ ansel.py
@@ -0,0 +1,22 @@
+# word = input('введите слово: ')
+# print('\nVсе буквы вашего слова по порядку:')
+# for letter in word:
+#     print (letter)
+
+# print("Посчитаем:")
+# for i in range(10):
+#     print(i, end= ' ')
+# print('\n\nПеречислим кратные пяти: ')
+# for i in range(0, 50, 5):
+#     print(i, end=' ')
+# print('\n\nПосчитаем в обратной порядке: ')
+# for i in range(10, 0, -1):
+#     print(i, end=' ')
+
+
+# l = list('13245')
+# l.sort()
+# print(' '.join(l))
+
+
+
